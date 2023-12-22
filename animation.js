b1c29ccb2817bf32aaac3ba2b892aec2d0bbe97a
Joanne F <115743659+joannefan@users.noreply.github.com>
2023-12-22 17:19:08 -0500

    banner

---
+++ animation.js
@@ -0,0 +1,72 @@
+function animateSeeds() {
+    const numberOfSeeds = 30;
+    const seedRadius = 25; // Half of the image width
+    const container = d3.select(".dandelions");
+
+    const seedData = Array.from({ length: numberOfSeeds }, (_, i) => {
+        const initialX = container.node().offsetWidth * 0.69; 
+        const initialY = container.node().offsetWidth * 0.1; 
+        const angle = Math.random() * (55 - (-30)) + (-30); // Random angle between -30 and 45 degrees
+        const radianAngle = (angle * Math.PI) / 180;
+        const vx = Math.cos(radianAngle) * (Math.random() * 2 - 1);
+        const vy = Math.sin(radianAngle) * (Math.random() * 2 - 1);
+        const animationDuration = Math.random() * (10000 - 6000) + 6000; // Random duration between 6000 and 10000 milliseconds
+
+        return {
+            id: i,
+            x: initialX,
+            y: initialY,
+            vx: vx,
+            vy: vy,
+            angle: angle,
+            animationDuration: animationDuration
+        };
+    });
+
+    const svg = container
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%");
+
+    const seeds = svg
+        .selectAll(".seed")
+        .data(seedData)
+        .enter()
+        .append("image")
+        .attr("class", "seed")
+        .attr("xlink:href", "images/seed.png") 
+        .attr("width", 50)
+        .attr("height", 50)
+        .attr("x", d => d.x - seedRadius) 
+        .attr("y", d => d.y - seedRadius)
+        .attr("transform", d => `rotate(${d.angle},${d.x},${d.y})`) // Rotate the image based on the random angle
+        .style("opacity", 0.8); 
+
+    function animateSeeds() {
+        seeds
+            .transition()
+            .duration(d => d.animationDuration)
+            .delay((_, i) => i * 200)
+            .attrTween("x", bounceX)
+            .attrTween("y", bounceY)
+            .on("end", animateSeeds);
+    }
+
+    animateSeeds();
+
+    function bounceX(d) {
+        return function (t) {
+            d.x += d.vx;
+            d.vx *= (d.x > container.node().offsetWidth - seedRadius || d.x < seedRadius) ? -1 : 1;
+            return d.x - seedRadius;
+        };
+    }
+
+    function bounceY(d) {
+        return function (t) {
+            d.y += d.vy;
+            d.vy *= (d.y > container.node().offsetHeight - seedRadius || d.y < seedRadius) ? -1 : 1;
+            return d.y - seedRadius;
+        };
+    }
+}
