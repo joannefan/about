3cacdf458d5a27862c4a25748c0111a06a07ac4e
Joanne <115743659+joannefan@users.noreply.github.com>
2024-05-29 02:32:52 -0400

    redo homepage, change nav styling

--- static.js
+++ static.js
@@ -1,22 +1,27 @@
 
 function getNavHtml() {
     const html = 
-    `<div class="navbar">
-        <div class="logo"></div>
-        <div class="nav-links">
-            <a href="index.html" class="btn draw-border">Home</a>
-            <a href="about.html" class="btn draw-border">About</a>
-            <a href="experience.html" class="btn draw-border">Experience</a>
-            <a href="portfolio.html" class="btn draw-border">Portfolio</a>
-        </div>
-        <img src="images/menu.png" class="menu">
-        <div class="mobile-nav-links">
-            <a href="index.html" class="mobile-btn">Home →</a>
-            <a href="about.html" class="mobile-btn">About →</a>
-            <a href="experience.html" class="mobile-btn">Experience →</a>
-            <a href="portfolio.html" class="mobile-btn">Portfolio →</a>
-        </div>
-    </div>`;
+    `<nav>
+      <ul>
+        <li>
+          <a href="index.html" class="btn draw-border">Home</a>
+        </li>
+        <li>
+          <a href="about.html" class="btn draw-border">About</a>
+        </li>
+        <li>
+          <a href="experience.html" class="btn draw-border">Experience</a>
+        </li>
+        <li>
+          <a href="portfolio.html" class="btn draw-border">Portfolio</a>
+        </li>
+        <li class="hamburger">
+          <a href="#">
+            <div class="bar"></div>
+          </a>
+        </li>
+      </ul>
+    </nav>`;
 
     return html;
 }
