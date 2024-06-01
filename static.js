d6b97da91769d42b11461f50db1116b444b6987d
Joanne <115743659+joannefan@users.noreply.github.com>
2024-06-01 04:03:46 -0400

    add github in nav, footer, update experience

--- static.js
+++ static.js
@@ -16,7 +16,7 @@ function getNavHtml() {
           <a href="project.html" class="btn draw-border">Projects</a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/joannefan/" target="_blank" class="btn draw-border">Contact</a>
+          <a href="https://github.com/joannefan" target="_blank" class="btn draw-border">GitHub</a>
         </li>
         <li class="hamburger">
           <a href="#">
@@ -32,7 +32,7 @@ function getNavHtml() {
 function getSidebarHtml(color) {
     const html = 
     `<div class="social-links">
-        <a href="https://www.linkedin.com/in/joannefan/" target="_blank">
+        <a href="https://github.com/joannefan" target="_blank">
             <img src="images/linkedin_${color}.png" alt="LinkedIn logo">
         </a>
         <a href="https://github.com/joannefan/" target="_blank">
