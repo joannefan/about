a45611f3e290127d1f88af894ef7f44e7f6ce61b
Joanne <115743659+joannefan@users.noreply.github.com>
2024-08-28 00:15:50 -0400

    updated content from resume

---
+++ workData.js
@@ -0,0 +1,33 @@
+const workEntriesList = [
+  {
+    "title": "Kathalyst",
+    "date": "May 2024 - July 2024",
+    "role": "Software Engineering Intern",
+    "description": [
+      "Automated new feature deployment at an early-stage startup by setting up a GitHub Action pipeline for continuous delivery.",
+      "Improved error detection by integrating Grafana with an Azure environment to monitor application metrics.",
+      "Optimized application architecture by profiling and implementing batch queries for PostgreSQL databases, achieving over 50% improvement in loading speed.",
+      "Developed a retrieval-augmented generation (RAG) pipeline using LangChain and Hugging Face to enhance context retrieval for large language models."
+    ]
+  },
+  {
+    "title": "Human Interaction Lab (Tufts)",
+    "date": "Jan 2024 - May 2024",
+    "role": "Research Assistant",
+    "description": [
+      "Enhanced the GailBot Python package by implementing support for multimedia formats and AWS S3 object interaction.",
+      "Led the prototyping of a new user interface within the GailBot PyQt desktop application for interacting with a backend API.",
+      "Debugged and tested the application in preparation for a UCLA workshop with Conversation Analysis researchers."
+    ]
+  },
+  {
+    "title": "Tufts Technology Services",
+    "date": "May 2023 - Aug 2023",
+    "role": "Data Analytics Intern",
+    "description": [
+      "Extended a data pipeline with Linux Bash scripts to automate backups.",
+      "Configured custom Prometheus metrics and alerts and created Grafana dashboards for real-time performance monitoring.",
+      "Implemented Golang scripts to process raw JSON data and crafted interactive D3.js visualizations to synthesize data insights."
+    ]
+  }
+];
